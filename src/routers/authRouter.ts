3cfe4263433a939e4c25810f3497e1498ceb4645
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-10 18:59:07 -0300

    implementa authSchema

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,8 +1,10 @@
 import {Router} from "express";
+import validateSchema from "../middlewares/validateSchema";
+import authSchema from "../schemas/authSchema";
 
 const authRouter = Router();
 
-authRouter.post('/signup');
-authRouter.post('/login');
+authRouter.post('/signup', validateSchema(authSchema));
+authRouter.post('/login', validateSchema(authSchema));
 
 export default authRouter;
