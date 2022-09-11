a58d5061afcd986e25730c9ccb73c8a75084db74
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-11 16:25:55 -0300

    implementa noteRepository

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,5 +1,7 @@
-import { credentials, users } from "@prisma/client";
+import { credentials, notes, users } from "@prisma/client";
 
 export type TypeAuth = Omit<users, 'id'>
 
 export type TypeCredential = Omit<credentials, 'id' | 'userId'>
+
+export type TypeNote = Omit<notes, 'id' | 'userId'>
