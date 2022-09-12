273993e336c2c1e9d9f52490d98e634c660248d2
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-12 04:35:16 -0300

    implementa wifiSchema

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,4 +1,4 @@
-import { cards, credentials, notes, users } from "@prisma/client";
+import { cards, credentials, notes, users, wifis } from "@prisma/client";
 
 export type TypeAuth = Omit<users, 'id'>
 
@@ -7,3 +7,5 @@ export type TypeCredential = Omit<credentials, 'id' | 'userId'>
 export type TypeNote = Omit<notes, 'id' | 'userId'>
 
 export type TypeCard = Omit<cards, 'id' | 'userId'>
+
+export type TypeWifi = Omit<wifis, 'id' | 'userId'>
