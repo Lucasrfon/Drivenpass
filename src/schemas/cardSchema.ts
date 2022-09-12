0e08e890ba03f228b4d0e00814b658742eb3cda7
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-12 14:52:27 -0300

    ajusta cardSchema

--- src/schemas/cardSchema.ts
+++ src/schemas/cardSchema.ts
@@ -1,13 +1,16 @@
 import joi from "joi";
+import joiDate from '@joi/date';
+const extendedJoi = joi.extend(joiDate)
 
 const cardSchema = joi.object({
-    number: joi.string().required(),
-    name: joi.string().required(),
-    cvv: joi.string().length(3).required(),
-    expiration: joi.date().required(),
+    number: joi.string().creditCard().required(),
+    name: joi.string().pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ ]*$/).required(),
+    cvv: joi.string().pattern(/^[0-9]{3}$/).required(),
+    expiration: extendedJoi.date().format('MM/YY').required(),
     password: joi.string().required(),
     isVirtual: joi.boolean().required(),
-    type: joi.string().valid('crédito', 'débito', 'ambos').required()
+    typeCard: joi.string().valid('crédito', 'débito', 'ambos').required(),
+    title: joi.string().required()
 });
 
 export default cardSchema
